ddfc49290b8aa581b76a3299cb24bcd0194f5c03
Akshay Joshi <joshi.a@gmail.com>
2009-09-25 22:33:40 +0400

    Added named scopes for the field model so that it's easier to retrieve the primary field for a fact.

--- app/models/field.rb
+++ app/models/field.rb
@@ -2,4 +2,7 @@
 class Field < ActiveRecord::Base
   belongs_to :fact
   belongs_to :field_model
+  
+  named_scope :fact, lambda { |*args| {:conditions => ['fact_id = ?', args.first]} }
+  named_scope :primary, :include => :fact, :conditions => ['facts.primary_field_id = fields.id']
 end
