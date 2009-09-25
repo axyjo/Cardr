15abeb918d11daeee99b10e213f464d3eb8977af
Akshay Joshi <joshi.a@gmail.com>
2009-09-25 14:43:31 +0400

    Added one-liner description to fact and field models.

--- app/models/fact.rb
+++ app/models/fact.rb
@@ -1,3 +1,4 @@
+# A single fact consisting of multiple fields.
 class Fact < ActiveRecord::Base
   has_many :fields
   belongs_to :deck
--- app/models/field.rb
+++ app/models/field.rb
@@ -1,3 +1,4 @@
+#A field in a fact.
 class Field < ActiveRecord::Base
   belongs_to :fact
   belongs_to :field_model
