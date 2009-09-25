eb2e10e73d3470db1f977f91a7c02a0829d913ab
Akshay Joshi <joshi.a@gmail.com>
2009-09-25 22:32:52 +0400

    Fixed table structure for fields and facts tables.

--- db/migrate/20090924204244_create_fields.rb
+++ db/migrate/20090924204244_create_fields.rb
@@ -2,7 +2,7 @@ class CreateFields < ActiveRecord::Migration
   def self.up
     create_table :fields do |t|
       t.column :fact_id, :integer
-      t.column :field_model_id, :integer
+      t.column :representation_id, :integer
       t.column :value, :text
       t.timestamps
     end
--- db/migrate/20090924204828_create_facts.rb
+++ db/migrate/20090924204828_create_facts.rb
@@ -2,6 +2,7 @@ class CreateFacts < ActiveRecord::Migration
   def self.up
     create_table :facts do |t|
       t.column :deck_id, :integer
+      t.column :primary_field_id, :integer
       t.column :space_until, :float
       t.timestamps
     end
