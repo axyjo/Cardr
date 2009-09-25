f0d97bd5f1fe52a9a4a7055753f742dcf6fa98e9
Akshay Joshi <joshi.a@gmail.com>
2009-09-25 22:36:45 +0400

    Added decks controller and the default-ish views for it.

---
+++ app/controllers/decks_controller.rb
@@ -0,0 +1,59 @@
+class DecksController < ApplicationController
+  before_filter :login_required
+  def index
+    @decks = Deck.all
+    
+    respond_to do |format|
+      format.html
+      format.xml { render :xml => @decks }
+    end
+  end
+  
+  def show
+    @deck = Deck.find(params[:id])
+    
+    respond_to do |format|
+      format.html
+      format.xml { render :xml => @deck }
+    end
+  end
+  
+  def new
+    @deck = Deck.new
+    @models = Model.find(:all, :order => :name)
+    respond_to do |format|
+      format.html
+      format.xml { render :xml => @deck }
+    end
+  end
+  
+  def create
+    @deck = Deck.new(params[:deck])
+    respond_to do |format|
+      if @deck.save
+        flash[:notice] = 'Deck was successfully created.'
+        format.html { redirect_to(@deck) }
+        format.xml  { render :xml => @deck, :status => :created, :location => @deck }
+      else
+        format.html { render :action => "new" }
+        format.xml  { render :xml => @deck.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+  
+  def edit
+  end
+  
+  def update
+  end
+  
+  def destroy
+    @deck = Deck.find(params[:id])
+    @deck.destroy
+    respond_to do |format|
+      flash[:notice] = "Deck <em>"+@deck.name+"</em> was deleted."
+      format.html { redirect_to(decks_url) }
+      format.xml  { head :ok }
+    end
+  end
+end
---
+++ test/functional/decks_controller_test.rb
@@ -0,0 +1,8 @@
+require 'test_helper'
+
+class DecksControllerTest < ActionController::TestCase
+  # Replace this with your real tests.
+  test "the truth" do
+    assert true
+  end
+end
